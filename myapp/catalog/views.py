76e8cf651d945feda55b1f846ad48bc3e68049ce
macbookpro <kwp@tlen.pl>
2023-05-25 13:24:10 +0200

    25.05 13:24

--- myapp/catalog/views.py
+++ myapp/catalog/views.py
@@ -1,54 +1,84 @@
-from flask import request, jsonify, Blueprint
-from myapp import db
+from functools import wraps
+
+from flask import request, jsonify, Blueprint, render_template, flash, redirect, url_for
+from myapp import db, app
 from myapp.catalog.models import Product, Category
 
 catalog = Blueprint('catalog', __name__)
 
+
+def template_or_json(template=None):
+    def decorated(f):
+        @wraps(f)
+        def decorated_fn(*args, **kwargs):
+            ctx = f(*args, **kwargs)
+            if request.is_json or not template:
+                return jsonify(ctx)
+            else:
+                return render_template(template, **ctx)
+
+        return decorated_fn
+
+    return decorated
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @catalog.route('/')
 @catalog.route('/katalog')
+# @template_or_json('home.html')
 def home():
-    return "Welcome to the catalog"
+    if request.is_json:
+        products = Product.query.all()
+        return jsonify({
+            'count': len(products)
+        })
+    # products = Product.query.all()
+    # return {'count': len(products)}
+    return render_template('home.html')
 
 
 @catalog.route('/products')
+@catalog.route('/products/<int:page>')
 def products():
-    products = Product.query.all()
-    res = {}
-    for product in products:
-        res[product.id] = {
-            'name': product.name,
-            'price': product.price,
-            'category': product.category.name
-        }
-    return jsonify(res)
+    products = Product.query.paginate(page=1, max_per_page=10)
+    return render_template('products.html', products=products)
 
 
 @catalog.route('/product/<id>')
 def product(id):
     product = Product.query.get_or_404(id)
-    return 'Product - %s, %s' % (product.name, product.price)
+    return render_template('product.html', product=product)
 
 
-@catalog.route('/product-create', methods=['POST', ])
+@catalog.route('/product-create', methods=['POST', 'GET'])
 def create_product():
-    name = request.form.get('name')
-    price = request.form.get('price')
-    categ_name = request.form.get('category')
-    category = Category.query.filter_by(name=categ_name).first()
-    if not category:
-        category = Category(categ_name)
-    product = Product(name, price, category)
-    db.session.add(product)
-    db.session.commit()
-    return 'Product created.'
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        categ_name = request.form.get('category')
+        category = Category.query.filter_by(name=categ_name).first()
+        if not category:
+            category = Category(categ_name)
+        product = Product(name, price, category)
+        db.session.add(product)
+        db.session.commit()
+        flash('The product %s has been created' % name, 'success')
+        return redirect(url_for('catalog.product', id=product.id))
+    return render_template('product-create.html')
 
-@catalog.route('/category-create', methods=['POST',])
+
+@catalog.route('/category-create', methods=['POST', ])
 def create_category():
     name = request.form.get('name')
     category = Category(name)
     db.session.add(category)
     db.session.commit()
     return 'Category created'
+
 
 @catalog.route('/categories')
 def categories():
@@ -64,4 +94,3 @@ def categories():
                 'name': product.name,
                 'price': product.price
             }
-    return jsonify(res)
